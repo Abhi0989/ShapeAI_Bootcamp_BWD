41035525f04cad63f3d0c0ac614003b2bf4826f0
Abhi0989 <85437235+Abhi0989@users.noreply.github.com>
2021-06-27 15:24:57 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer() {
+return (
+<footer>
+ <p>Cpoyright by ShapeAI @ {new Date().getFullYear()}</p>
+</footer>
+);
+}
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+return (
+<div className="note">
+ <h1>JavaScript And React.js</h1>
+ <p >Basic Web Dev React js Bootcamp</p>
+</div>
+);
+}
+export default Info;
